17be30aebb8607345ce05664f92c6e52fa08db7c
ernesthenry <henry38ernest@gmail.com>
2024-08-14 18:08:51 +0300

    Display flash cards

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -15,8 +15,9 @@ export default function Flashcard() {
     const [flashcards, setFlashcards] = useState([])
     const router = useRouter()
   
-    // ... (rest of the component)
-
+    const handleCardClick = (id) => {
+        router.push(`/flashcard?id=${id}`)
+      }
     useEffect(() => {
         async function getFlashcards() {
           if (!user) return
